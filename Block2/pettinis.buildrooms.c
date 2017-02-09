7c264507a2c2b140df2fb7c8ec586864a91b405f
Sammy Pettinichi <xeratun@users.noreply.github.com>
2017-02-09 11:17:08 -0500

    Update pettinis.buildrooms.c

--- Block2/pettinis.buildrooms.c
+++ Block2/pettinis.buildrooms.c
@@ -16,28 +16,33 @@ struct Room {
 };
 
 char* getName(int randNum, int usedRooms[]){
+	char name[25];
 	if (randNum == 0 && usedRooms[0] == 0)
-		return "EDINBURGH\0";
-	else if (randNum == 1 && usedRooms[1] == 1)
-		return "CORNWALL\0";
-	else if (randNum == 2 && usedRooms[2] == 2)
-		return "CARDIFF\0";
-	else if (randNum == 3 && usedRooms[3] == 3)
-		return "BRISTOL\0";
-	else if (randNum == 4 && usedRooms[4] == 4)
-		return "YORK\0";
-	else if (randNum == 5 && usedRooms[5] == 5)
-		return "KENT\0";
-	else if (randNum == 6 && usedRooms[6] == 6)
-		return "OXFORD\0";
-	else if (randNum == 7 && usedRooms[7] == 7)
-		return "SANDFORD\0";
-	else if (randNum == 8 && usedRooms[8] == 8)
-		return "CANTERBURY\0";
-	else if (randNum == 9 && usedRooms[9] == 9)
-		return "LONDON\0";
-	printf("Error, no names available or invalid input.\n");
-	return "ERROR\0";
+		name = "EDINBURGH\0";
+	else if (randNum == 1 && usedRooms[1] == 0)
+		name = "CORNWALL\0";
+	else if (randNum == 2 && usedRooms[2] == 0)
+		name = "CARDIFF\0";
+	else if (randNum == 3 && usedRooms[3] == 0)
+		name = "BRISTOL\0";
+	else if (randNum == 4 && usedRooms[4] == 0)
+		name = "YORK\0";
+	else if (randNum == 5 && usedRooms[5] == 0)
+		name = "KENT\0";
+	else if (randNum == 6 && usedRooms[6] == 0)
+		name = "OXFORD\0";
+	else if (randNum == 7 && usedRooms[7] == 0)
+		name = "SANDFORD\0";
+	else if (randNum == 8 && usedRooms[8] == 0)
+		name = "CANTERBURY\0";
+	else if (randNum == 9 && usedRooms[9] == 0)
+		name = "LONDON\0";
+	else{
+		printf("Error, no names available or invalid input.\n");
+		name = "ERROR\0";
+	}
+	usedRooms[randNum] = 1;
+	return name;
 }
 
 struct Room* makeRoom(){
