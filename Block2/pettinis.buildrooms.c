da507ce2c855e9356b16f252fe1a724293906544
Sammy Pettinichi <xeratun@users.noreply.github.com>
2017-02-10 11:42:20 -0500

    Update pettinis.buildrooms.c

--- Block2/pettinis.buildrooms.c
+++ Block2/pettinis.buildrooms.c
@@ -74,10 +74,10 @@ int main(void){
 	srand((unsigned) time(&t));
 	struct Room *rooms[numRooms];
 	rooms[0] = makeRoom(usedRooms);
-	printf("Main: %s\n",rooms[i]->name);
+	printf("Main: %s\n",rooms[0]->name);
 	sprintf(rooms[0]->type,"START_ROOM");
 	rooms[1] = makeRoom(usedRooms);
-	printf("Main: %s\n",rooms[i]->name);
+	printf("Main: %s\n",rooms[1]->name);
 	sprintf(rooms[1]->type,"END_ROOM");
 	for (i=2; i<numRooms; i++){
 		rooms[i] = makeRoom(usedRooms);
