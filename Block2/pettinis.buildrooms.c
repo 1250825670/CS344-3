3f664a8dec185af0977f0377d52089df5511b690
Sammy Pettinichi <xeratun@users.noreply.github.com>
2017-02-10 09:36:20 -0500

    Update pettinis.buildrooms.c

--- Block2/pettinis.buildrooms.c
+++ Block2/pettinis.buildrooms.c
@@ -15,44 +15,44 @@ struct Room {
 	char type[25];
 };
 
-char* getName(int randNum, int usedRooms[]){
-	if (randNum == 0 && *usedRooms[0] == 0){
+char* getName(int randNum, int* usedRooms){
+	if (randNum == 0 && usedRooms[0] == 0){
 		usedRooms[randNum] = 1;
 		return "EDINBURGH\0";
 	}
-	else if (randNum == 1 && *usedRooms[1] == 0){
+	else if (randNum == 1 && usedRooms[1] == 0){
 		usedRooms[randNum] = 1;
 		return "CORNWALL\0";
 	}
-	else if (randNum == 2 && *usedRooms[2] == 0){
+	else if (randNum == 2 && usedRooms[2] == 0){
 		usedRooms[randNum] = 1;
 		return "CARDIFF\0";
 	}
-	else if (randNum == 3 && *usedRooms[3] == 0){
+	else if (randNum == 3 && usedRooms[3] == 0){
 		usedRooms[randNum] = 1;
 		return "BRISTOL\0";
 	}
-	else if (randNum == 4 && *usedRooms[4] == 0){
+	else if (randNum == 4 && usedRooms[4] == 0){
 		usedRooms[randNum] = 1;
 		return "YORK\0";
 	}
-	else if (randNum == 5 && *usedRooms[5] == 0){
+	else if (randNum == 5 && usedRooms[5] == 0){
 		usedRooms[randNum] = 1;
 		return "KENT\0";
 	}
-	else if (randNum == 6 && *usedRooms[6] == 0){
+	else if (randNum == 6 && usedRooms[6] == 0){
 		usedRooms[randNum] = 1;
 		return "OXFORD\0";
 	}
-	else if (randNum == 7 && *usedRooms[7] == 0){
+	else if (randNum == 7 && usedRooms[7] == 0){
 		usedRooms[randNum] = 1;
 		return "SANDFORD\0";
 	}
-	else if (randNum == 8 && *usedRooms[8] == 0){
+	else if (randNum == 8 && usedRooms[8] == 0){
 		usedRooms[randNum] = 1;
 		return "CANTERBURY\0";
 	}
-	else if (randNum == 9 && *usedRooms[9] == 0){
+	else if (randNum == 9 && usedRooms[9] == 0){
 		usedRooms[randNum] = 1;
 		return "LONDON\0";
 	}
@@ -62,7 +62,7 @@ char* getName(int randNum, int usedRooms[]){
 	}
 }
 
-struct Room* makeRoom(int& usedRooms[]){
+struct Room* makeRoom(int* usedRooms){
 	struct Room* room = (struct Room*) malloc(sizeof(struct Room));
 	sprintf(room->name,getName(rand() % 10,usedRooms));
 	room->totalConnections = (rand() % 4) + 3;
@@ -111,7 +111,7 @@ int main(void){
 	for(i=0;i<10;i++)
 		usedRooms[i] = 0;
 	for (i=2; i<(numRooms+3); i++){
-		rooms[i] = makeRoom(usedRooms);
+		rooms[i] = makeRoom(&usedRooms);
 	}
 	printf("Rooms created\n");
 	char directory[100];
