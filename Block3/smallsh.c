85ef1e93aed0c5dd52b0c578b7687762a0018ea2
Sammy Pettinichi <xeratun@users.noreply.github.com>
2017-02-16 15:38:19 -0500

    Update smallsh.c

--- Block3/smallsh.c
+++ Block3/smallsh.c
@@ -1,4 +1,10 @@
-//ssting to parse fgets data
+//sstring to parse fgets data
+//use read/write for re-entrance
+
+/* Sammy Pettinichi
+ * CS344 - Operating Systems
+ * Project 3 - smallsh
+ */
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -10,21 +16,31 @@
 #include <signal.h>
 #include <fcntl.h>
 
+//define constants
 #define MAX_INPUT 2048
 #define MAX_COMMANDS 512
 
+//function declarations
 void print(char* text);
-char** getInput(int* background);
-char** parseInput(char input[]);
-void executeCommand(char** commandArguments, int* exitVal, int* background);
+char** getInput(int* background, int* counter);
+char** parseInput(char input[], int* counter);
+void executeCommand(char** commandArguments, int* exitVal, int* background, int* counter);
 
-void executeCommand(char** commandArguments, int* exitVal, int* background){  
+//execute the user's command
+void executeCommand(char** commandArguments, int* exitVal, int* background, int* counter){  
+  char printOutput[MAX_INPUT];
+  int i,file=-1;
+  int outSlot=0,inSlot=0;
+  memset(printOutput,'\0',sizeof(printOutput));
   if(strcmp(commandArguments[0],"status")==0){
-    if(*exitVal == 0 || *exitVal == 1)
-      puts("exit value %d\n",*exitVal);
-    else
-      puts("terminated by signal %d\n",*exitVal);
-    fflush(stdout);
+    if(*exitVal == 0 || *exitVal == 1){
+      sprintf(printOutput,"exit value %d\n",*exitVal);
+      print(printOutput);
+    }
+    else{
+      sprintf(printOutput,"terminated by signal %d\n",*exitVal);
+      print(printOutput);
+    }
     *exitVal = 0;
   }
   else if(strcmp(commandArguments[0],"cd")==0){
@@ -34,40 +50,70 @@ void executeCommand(char** commandArguments, int* exitVal, int* background){
       chdir(commandArguments[1]);
     *exitVal = 0;
   }
+  else if(count > 1){
+    for(i=0;i<*count;i++){
+      if(strcmp(commandArguments[i],"<")==0){
+        inSlot = i;
+      }
+      else if(strcmp(commandArguments[i],">")==0){
+        outSlot = i;
+      }
+    }
+    if(inSlot != 0){
+      file = open(commandArugments[inSlot+1],O_RDONLY);
+      if(file == -1){
+        spritnf(printOutput,"cannot open %s\n",commandArguments[inSlot+1]);
+        print(pritnOutput);
+        *exitVal = 1;
+      }
+    }
+    else if(outSlot != 0){
+      file = open(commandArguments[0],O_CREAT);
+      if(file == -1){
+        spritnf(printOutput,"cannot open %s\n",commandArguments[0]);
+        print(pritnOutput);
+        *exitVal = 1;
+      }
+    }
+    else{
+      sprintf(printOutput,"%s: no such file or directory\n",commandArguments[0]);
+      print(printOutput);
+    }
+  }
   else{
-    if(
-    
-    puts("%s: no such file or directory\n",commandArguments[0]);
-    fflush(stdout);
+    //standalone exec command
   }
 }
 
 int main(int argc, char *argv[]){
   char** commandArguments;
-  int exitVal, background;
+  int exitVal,background,counter;
   do{
     background = 0;
-    commandArguments = getInput(input, &background);
+    counter = 0;
+    commandArguments = getInput(input, &background,&counter);
     
-    if(commandArguments[0] != 0 && strcmp(commandArguments[0],"exit") != 0){
-      executeCommand(commandArguments, &exitVal, &background);
+    if(counter != 0 && strcmp(commandArguments[0],"exit") != 0){
+      executeCommand(commandArguments, &exitVal, &background,&counter);
     }
     free(commandArguments);
   }while(strcmp(commandArguments[0],"exit") != 0);
 }
 
-char** getInput(int* background){
+//request command input from the user
+char** getInput(int* background, int* counter){
   char input[MAX_INPUT];
   memset(intput,'\0',sizeof(input));
   print(": ");
   fflush(stdin);
+  read(0,input,sizeof(input));
   fgets(input, MAX_INPUT, stdin)
   
-  return parseInput(input,background);
+  return parseInput(input,background,counter);
 }
 
-char** parseInput(char input[], int* background){
-  int counter;
+//parse the input into an array for easier access
+char** parseInput(char input[], int* background, int* counter){
   char** commandArguments = malloc(sizeof(char *) * MAX_COMMANDS);
   char command[MAX_INPUT];
   char* token;
@@ -79,20 +125,20 @@ char** parseInput(char input[], int* background){
     return commandArguments;
   }
   strcpy(command, input);
-  counter = 0;
   token = strtok(command, " ");
   while(token != NULL){
     strcpy(commandArguments[counter],token);
-    counter++;
+    *counter++;
     token = strtok(NULL, " ");
   }
-  if(strcmp(commandArugments[counter-1],"&")==0){
+  if(strcmp(commandArugments[*counter-1],"&")==0){
     *background = 1;
   }
   return commandArguments;
 }
 
+//write codes: 0=stdin, 1=stdout, 2=stderr
 void print(char* text){
-  puts(text);
+  write(1,text,sizeof(text));
   fflush(stdout);
 }
