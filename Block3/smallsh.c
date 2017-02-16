e84e83618cc857dcff6aeed4bc7b765c3b98b056
Sammy Pettinichi <xeratun@users.noreply.github.com>
2017-02-16 11:06:34 -0500

    Update smallsh.c

--- Block3/smallsh.c
+++ Block3/smallsh.c
@@ -1,77 +1,94 @@
+//ssting to parse fgets data
+
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <signal.h>
+#include <fcntl.h>
 
 #define MAX_INPUT 2048
+#define MAX_COMMANDS 512
 
 void print(char* text);
-char* getInput();
-void getFirstWord(char firstWord[], char* input);
-runStatus(char* input);
-runCD(char* input);
-runMisc(char* input);
+char** getInput();
+char** parseInput(char input[]);
+void executeCommand(char** commandArguments);
+void runStatus(char input[]);
+void runCD(char* input);
+void runMisc(char* input);
 
-runMisc(char* input){
-  
+void executeCommand(char** commandArguments){
+  if(strcmp(commandArguments[0],"status")==0){
+    runStatus(commandArguments);
+  }
+  else if(strcmp(commandArguments[0],"cd")==0){
+    runCD(commandArguments);
+  }
+  else{
+    runMisc(commadArguments); 
+  }
+}
+void runMisc(char* input){
+  //exec() family of commands
   else
     print("unknown command\n");
 }
 
-runStatus(char* input){
+void runStatus(char* input){
   
 }
 
-runCD(char* input){
+void runCD(char* input){
   
-}
-
-char* getInput(){
-  char input[MAX_INPUT];
-  memset(intput,'\0',sizeof(input));
-  print(": ");
-  fgets(input, MAX_INPUT, stdin)
-  
-  char *c = input;
-  int spaces=0;
-  while (*c == ' ') || (*c == '\t'){
-    c++;
-    spaces++;
-  }
-  c = input;
-  if(*c == '#' || *c == ' ' || c == '\t' || sizeof(input) == 0){
-    return getInput();
-  }
-  return input;
 }
 
 int main(int argc, char *argv[]){
-char firstWord[50];
-char input[MAX_INPUT];
   do{
-    memset(intput,'\0',sizeof(input));
-    input = getInput();
-    getFirstWord(firstWord, input);
+    char** commandArguments = getInput(input);
     
-    if(strcmp(firstWord,"status")==0){
-      runStatus(input);
+    if(commandArguments[0] != 0 && strcmp(commandArguments[0],"exit") != 0){
+      executeCommand(commandArguments);
     }
-    else if(strcmp(firstWord,"cd")==0){
-      runCD(input);
-    }
-    else{
-      runMisc(input);
-    }
-  }while(strcmp(input,"exit") != 0);
+  }while(strcmp(commandArguments[0],"exit") != 0);
 }
 
-void getFirstWord(char firstWord[], char* input){
-  memset(firstWord,'\0',sizeof(firstWord));
-  int counter=0;
+char** getInput(){
+  char input[MAX_INPUT];
+  memset(intput,'\0',sizeof(input));
+  print("smallsh: ");
+  fgets(input, MAX_INPUT, stdin)
+  
+  return parseInput(input);
+}
+
+char** parseInput(char input[]){
+  int counter, i;
+  char** commandArguments = malloc(sizeof(char *) * MAX_COMMANDS);
+  char command[MAX_INPUT];
+  char* token;
+  memset(commandArguments,'\0',sizeof(commandArguments));
+  memset(command,'\0',sizeof(command));
   char *c = input;
-  while(c != ' '){
-    firstWord[counter] = *c;
-    c++;
-    counter++;
+  if(*c == '#' || *c == ' ' || c == '\t' || strlen(input) == 0){
+    commandArguments[0] = 0;
+    return commandArguments;
   }
+  strcpy(command, input);
+  counter = 0;
+  token = strtok(command, " ");
+  while(token != NULL){
+    strcpy(commandArguments[counter],token);
+    counter++;
+    token = strtok(NULL, " ");
+  }
+  for(i=1; i<strlen(input); i++){
+   
+  }
+  return commandArguments;
 }
 
 void print(char* text){
